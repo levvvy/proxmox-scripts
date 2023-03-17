a6a2c8442654a3a38d6ce1fd3ff7198353084a79
levvvy <levvvy@gmail.com>
2023-03-17 19:40:21 +0100

    Update launch_script.sh

--- main/launch_script.sh
+++ main/launch_script.sh
@@ -6,10 +6,3 @@ if (whiptail --title "Create LXC Container" --yesno "Do you want to create an LX
 else
   echo "LXC container creation skipped."
 fi
-
-# Ask for permission before proceeding to the second script
-if (whiptail --title "Install Snapcast Server" --yesno "Do you want to install Snapcast Server in the newly created container?" 8 78); then
-  bash -c "$(wget -qLO - https://raw.githubusercontent.com/levvvy/proxmox-scripts/main/install_snapcast_server.sh)"
-else
-  echo "Snapcast Server installation skipped."
-fi
